a1e0dbd8b4157f976b0c3dceccd2ffa1f52506f5
Satyam <satyamchauhan709@gmail.com>
2024-10-05 18:29:58 +0530

    recoil init

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,11 +3,12 @@ import client from "@repo/db/client2";
 
 export const GET = async () => {
     console.log("adding ...");
+    
   await client.user.create({
     data: {
-      email: "satyam1@gmail.com",
-      name: "satyam",
-      number: "111111111",
+      email: "satyam3@gmail.com",
+      name: "satyam3",
+      number: "111111112",
       password: "satyam",
     },
   });
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-
+"use client";
+import { useBalance } from "@repo/store/useBalance";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center bg-black text-white">
-    <div className="text-5xl font-serif">
-      Hey There
-    </div></div>
+      <div className="h-[100%] flex justify-center items-center bg-black text-white">
+        <div className="text-5xl font-serif">Hey There {useBalance()}</div>
+      </div>
   );
 }
